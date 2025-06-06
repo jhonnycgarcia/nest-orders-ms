bf5af25de6a26476586693979f69bd9c539362de
Jhonny <jhonnycgarcia@gmail.com>
2025-06-05 21:20:21 -0500

    Configurar orders microservice

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: envs.PORT,
+    }
+  });
   const logger = new Logger('Main');
-  await app.listen(envs.PORT);
+
   logger.log(`Microservice orders is running on port ${envs.PORT}`);
 }
 bootstrap();
