2d46c3beded17130bc3d05aaa4d48507d9ceecb6
Jhonny <jhonnycgarcia@gmail.com>
2025-06-06 23:40:41 -0500

    OrderItems - Detalles de la orden

---
+++ prisma/migrations/20250607043920_order_item/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "Order" ALTER COLUMN "status" DROP DEFAULT;
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "product" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "orderId" TEXT NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
