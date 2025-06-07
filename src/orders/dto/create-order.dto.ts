26114ed29eca77aa2b413e3c465a442fcba985d0
Jhonny <jhonnycgarcia@gmail.com>
2025-06-07 00:10:49 -0500

    DTOs de creación de orden

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,26 +1,31 @@
-import { IsBoolean, IsDate, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatus } from "generated/prisma";
-import { OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 export class CreateOrderDto {
-    @IsNumber()
-    @IsPositive()
-    @Type(() => Number)
-    totalAmount: number;
+    // @IsNumber()
+    // @IsPositive()
+    // @Type(() => Number)
+    // totalAmount: number;
 
-    @IsNumber()
-    @IsPositive()
-    @Type(() => Number)
-    totalItems: number;
+    // @IsNumber()
+    // @IsPositive()
+    // @Type(() => Number)
+    // totalItems: number;
 
-    @IsEnum(OrderStatusList, {
-        message: `Status must be one of the following: ${OrderStatusList.join(', ')}`,
-    })
-    @IsOptional()
-    status?: OrderStatus = OrderStatus.PENDING;
+    // @IsEnum(OrderStatusList, {
+    //     message: `Status must be one of the following: ${OrderStatusList.join(', ')}`,
+    // })
+    // @IsOptional()
+    // status?: OrderStatus = OrderStatus.PENDING;
 
-    @IsBoolean()
-    @IsOptional()
-    paid?: boolean = false;
+    // @IsBoolean()
+    // @IsOptional()
+    // paid?: boolean = false;
+
+    @IsArray()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true })
+    @Type(() => OrderItemDto)
+    items: OrderItemDto[];
 }
