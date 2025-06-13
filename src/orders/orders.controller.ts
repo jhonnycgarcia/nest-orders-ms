f37beb03bd62b1714b0ae19e83cb3800d218d68a
Jhonny <jhonnycgarcia@gmail.com>
2025-06-13 14:12:02 -0500

    EventPattern - Emitir eventos

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, NotImplementedException, ParseUUIDPipe } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
@@ -31,6 +31,12 @@ export class OrdersController {
   @MessagePattern('changeOrderStatus')
   changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto) {
     return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
+
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDtio: any) {
+    console.log({ paidOrderDtio });
+    return;
   }
 
 }
